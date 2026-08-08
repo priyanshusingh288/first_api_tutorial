11317a90a3f2d9e0fe1b560910fffb7819384b0a
priyanshusingh288 <you@example.com>
2026-08-08 23:10:13 +0530

    new changes to the query and other param

--- basic.py
+++ basic.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
@@ -101,3 +102,36 @@ def update_post(id: int, post: Product):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with this id:{id} not found")
 
     return {"data": updated_post}
+=======
+from fastapi import FastAPI,HTTPException
+from typing import Optional
+from pydantic import BaseModel
+from fastapi.params import Body
+
+app = FastAPI()
+
+items = []
+
+@app.get("/")
+def root():
+    return {"hello":"world"}
+
+@app.post("/items")
+def create_items(item:str):
+    items.append(item)
+    print(items)
+    return items
+
+@app.get("/items")
+def list_limits(limit:int = 10):
+    print(limit)
+    return items[0:limit]
+
+@app.get("/items/{item_id}")
+def get_items(item_id:int) -> str:
+    if item_id < len(items):
+        return items[item_id]
+    else:
+        raise HTTPException(status_code=404, detail = "item not found")
+    
+>>>>>>> 3929516 (new changes to the query and other param)
