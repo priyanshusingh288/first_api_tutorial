d3b821140599b504b70107bdd45bfc9148195f02
priyanshusingh <89io89p@gmail.com>
2026-08-23 23:05:41 +0530

    Add post creation and retrieval endpoints
    Refactor FastAPI application to include post management features.

--- basic.py
+++ basic.py
@@ -1,31 +1,44 @@
-from fastapi import FastAPI,HTTPException
-from typing import Optional
+from fastapi import FastAPI
 from pydantic import BaseModel
-from fastapi.params import Body
+from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
-items = []
+class Post(BaseModel):  
+    tittle: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+my_posts = [
+    {"title":"title of post 1","content":"contenet of contnet 1","id":1},
+    {"title":"favourite foods","content":"i love birani with sprite","id":2}
+]
+
+def search_post(post_id: int): 
+    for p in my_posts:
+        if p['id'] == post_id:  
+            return p
+    return None
 
 @app.get("/")
 def root():
     return {"hello":"world"}
 
-@app.post("/items")
-def create_items(item:str):
-    items.append(item)
-    print(items)
-    return items
+@app.get("/posts")
+def get_posts():
+    return {"this is your ": my_posts}
 
-@app.get("/items")
-def list_limits(limit:int = 10):
-    print(limit)
-    return items[0:limit]
+@app.post("/create_posts")
+def create_post(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0,100000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
 
-@app.get("/items/{item_id}")
-def get_items(item_id:int) -> str:
-    if item_id < len(items):
-        return items[item_id]
-    else:
-        raise HTTPException(status_code=404, detail = "item not found")
-    
+@app.get("/posts/{id}")
+def get_post(id: int):  
+    post = search_post(id)
+    print(post)
+    return {"post detail": post}
