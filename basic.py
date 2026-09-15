2c918bd59d0a8781ce426303ad099df7e2be8fd4
priyanshusingh <89io89p@gmail.com>
2026-09-15 22:14:51 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -16,7 +16,7 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 while True:
- try:     //implementing connection to my database using psycopg2//
+ try:     //implementing connection to my database using psycopg2 and doing alchemy CRUD//
     conn = psycopg2.connect(host = "localhost",database = "fastapi",user = "postgres",password = .... , cursor_factory=RealDictCursor)
     cursor = conn.cursor()
     print("database connected succesfully")
