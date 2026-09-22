f895dca790095202cec7fe197f32f28a72cf6ab9
priyanshusingh288 <you@example.com>
2026-09-23 01:20:32 +0530

    refactor raw sql to orm sqlalchemy layer

--- app/models.py
+++ app/models.py
@@ -1,7 +1,17 @@
-from database import Base
-from sqlalchemy import column,Integer,string,Boolean
+from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP
+from sqlalchemy.sql.expression import text
+from app.database import Base
 
-class post(Base):
+class Product(Base):
     __tablename__ = "products"
-    id = column(Integer, primary_key = True,nullable = False)
 
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    price = Column(Integer, nullable=False)
+    available = Column(Boolean, server_default="TRUE", nullable=False)
+    inventory = Column(Integer, server_default="0", nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=True),
+        nullable=False,
+        server_default=text("now()")
+    )
